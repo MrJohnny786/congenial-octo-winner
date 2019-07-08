a09b9ea34d5ee45841e6abb626f7497ee113442c
Johnny786 <johndamilatis786@gmail.com>
2019-07-09 02:30:12 +0300

    Random name bash script

--- bash_scripts/random_name.sh
+++ bash_scripts/random_name.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-input ""
+user=$(whoami)
+day=$(date +%A)
+path=$(pwd)
+
+file_c=/home/mrjohnny786/microservices/practice/congenial-octo-winner/resources/fixed_colors.txt
+file_o=/home/mrjohnny786/microservices/practice/congenial-octo-winner/resources/fixed_objects.txt
+file_a=/home/mrjohnny786/microservices/practice/congenial-octo-winner/resources/adjectives.txt
+#adj=$(sed -e /^$/d "$file_a")
+#col=$(sed -e /^$/d "$file_c")
+#obj=$(sed -e /^$/d "$file_o")
+
+
+col=$(shuf -n 1 $file_c)
+adj=$(shuf -n 1 $file_a)
+obj=$(shuf -n 1 $file_o)
+
+echo $col $adj $obj
--- python_scripts/fix_color_names.py
+++ python_scripts/fix_color_names.py
@@ -11,6 +11,10 @@ class Fixer(object):
         self.new_colors = []
         self.fixed_colors_path = '/home/mrjohnny786/microservices/practice/congenial-octo-winner/resources/fixed_colors.txt'
 
+        self.object_path = '/home/mrjohnny786/microservices/practice/congenial-octo-winner/resources/objects.txt'
+        self.new_objects = []
+        self.fixed_objects_path = '/home/mrjohnny786/microservices/practice/congenial-octo-winner/resources/fixed_objects.txt'
+
     def remove_color_codes(self, data):
         for color in data:
             _color = color.split()
@@ -26,11 +30,22 @@ class Fixer(object):
 
         return True
 
-    def create_new_file(self, data):
+    def clean_objects(self, data):
+        for object in data:
+            if object is '\n':
+                pass
+            else:
+                object = object.strip()
+                object = object.split()[1:]
+                object = ' '.join(object).lower()
+                self.new_objects.append(object)
 
-        new_file = open(self.fixed_colors_path, 'w')
-        for color in data:
-            new_file.write(color + "\n")
+        return True
+
+    def create_new_file(self, data, new_path):
+        new_file = open(new_path, 'w')
+        for thing in data:
+            new_file.write(thing + "\n")
 
         return True
 
@@ -41,7 +56,18 @@ class Fixer(object):
             raise Exception('No file found :(')
         if not self.remove_color_codes(colors_file):
             raise Exception("Something went wrong")
-        self.create_new_file(self.new_colors)
+
+        try:
+            objects_file = open(self.object_path, 'r')
+        except FileNotFoundError:
+            raise Exception('No file found :(')
+        if not self.clean_objects(objects_file):
+            raise Exception("Something went wrong")
+
+        self.create_new_file(self.new_colors, self.fixed_colors_path)
+        self.create_new_file(self.new_objects, self.fixed_objects_path)
+
+        print("Succesfully fixed object and color files :D")
 
 
 if __name__ == '__main__':
